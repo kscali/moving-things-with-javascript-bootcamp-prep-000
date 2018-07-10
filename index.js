4c701b4e29121ce4cf55483352eeb4e7f485d310
kosatun@aol.com <kosatun@aol.com>
2018-07-10 21:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
 }
 
 document.addEventListener('keydown', function(e) {
